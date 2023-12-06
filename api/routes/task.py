ab189d8db397be2e39b86d18f8550fc471868366
f-andrei <andreifrancap@gmail.com>
2023-12-06 13:28:04 -0300

    fix: removed unused imports

--- api/routes/task.py
+++ api/routes/task.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from database.models import Child, Task, db, ChildTask
+from database.models import  Task, db
 from datetime import datetime, time
 from dateutil.parser import parse as date_parse
 
